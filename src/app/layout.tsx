91ca89f862b0925b8334f2664c0b3cebd05e5a92
deepsky616 <climbing1126@gmail.com>
2026-04-16 15:04:09 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: '경기도교육청 학업성적관리규정 분석',
-  description: '경기도 초등학교 학업성적관리 시행지침 분석 도구',
+  description: '2026 경기도 초등학교 학업성적관리 시행지침 분석 도구',
 };
 
 export default function RootLayout({
